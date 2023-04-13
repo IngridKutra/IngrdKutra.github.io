fa535448ab117d062c1d40bfbb2ba9840b375c94
Ingrid <ingrid.lia.testing@gmail.com>
2023-04-13 20:23:34 +0200

    added another rest api

--- src/Startsida.jsx
+++ src/Startsida.jsx
@@ -4,19 +4,31 @@ import { NavLink } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import ReactStars from "react-rating-stars-component";
 
+
 // 2 Startsida
 const imgStyle = {
     width: "100%",
-    height: "450px",
-}
+    height: "200px",
+    objectFit: "cover",
+  }
 const receptLink = {
     textDecoration: "none",
-    color: "#8B6E4E"
+    color: "#8B6E4E",
+    
 }
+
+const titleStyle = {
+    fontSize: "1.2rem", // adjust the font size as needed
+    margin: "0",
+    lineHeight: "1.2",
+    fontWeight: "bold"
+  }
+
 const textStyle = {
     backgroundColor: "#E3D7C8",
     padding: "8px 16px",
-    height: "400px"
+    height: "200px",
+    fontSize: "16px",
 }
 
 const kategoriCardStyle = {
@@ -25,19 +37,20 @@ const kategoriCardStyle = {
 
 const Recept = ({ searchText }) => {
     const [receptList, setRecept] = useState([]);
-
     const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
         setIsLoading(true)
-        fetch(`https://paprika-bxu3y.ondigitalocean.app/recipes?query=${searchText}`)
+        fetch(`https://api.spoonacular.com/recipes/search?apiKey=9ac733f6a6314d859f64d2c2012285e2&query=${searchText}`)
             .then(res => res.json())
             .then(data => {
                 setIsLoading(false)
-                setRecept(data)
-
+                setRecept(data.results)
+                console.log("receptList: ", receptList)
             })
 
     }, [searchText])
+
+   
 
 // 2.7 Receptkort
 
@@ -47,25 +60,25 @@ const Recept = ({ searchText }) => {
                 {isLoading ?
                     <p>Laddar...</p> : receptList.length === 0 ?
                         <p>Inga bakverk hittades!</p> : receptList.map((recept) =>
-                            <Col sm={12} md={6} lg={4} key={recept._id} style={kategoriCardStyle}>
-                                <NavLink to={`/recipes/${recept._id}`} style={receptLink}>
-                                    <img alt={recept.title} src={recept.imageUrl} style={imgStyle} />
-                                    <div style={textStyle}>
-                                        <h2><b>{recept.title}</b>
-                                            <ReactStars
-                                                count={5}
-                                                edit={false}
-                                                size={35}
-                                                isHalf={true}
-                                                color={'#EAEEC5'}
-                                                activeColor={"#8B6E4E"}
-                                                value={recept.avgRating}
-                                            />   </h2>
-                                        <p> {recept.description}</p>
-                                        <b>{recept.categories} || {recept.timeInMins} Minuter</b>
-                                    </div>
-                                </NavLink>
-                            </Col>)}
+                        <Col sm={12} md={6} lg={4} key={recept.id} style={kategoriCardStyle}>
+                        <NavLink to={`/recipes/${recept.id}`} recipeId={recept.id} style={receptLink}>
+  <img alt={recept.title} src={`https://spoonacular.com/recipeImages/${recept.image}`} style={imgStyle} />
+  <div style={textStyle}>
+    <h2 style={titleStyle}><b>{recept.title}</b>
+      <ReactStars
+        count={5}
+        edit={false}
+        size={35}
+        isHalf={true}
+        color={'#EAEEC5'}
+        activeColor={"#8B6E4E"}
+        value={recept.rating}
+      />   
+    </h2>
+    <b>{recept.readyInMinutes} Minuter</b>
+  </div>
+</NavLink>
+                    </Col>)}
             </Row>
         </Container>
     )
